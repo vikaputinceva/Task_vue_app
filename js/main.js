5772ae291db048b32ef07d47ccf29c2ad3eb65e8
vikaputinceva <vikaputin2008@gmail.com>
2026-02-04 21:37:23 +0700

    practik4

--- js/main.js
+++ js/main.js
@@ -7,7 +7,19 @@ let app = new Vue ({
         altText: "A pair of socks",
         //link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
         inStock: true,
-        inventory: 100,
-        onSale: true,
+        //inventory: 100,
+        //onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [ 
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
