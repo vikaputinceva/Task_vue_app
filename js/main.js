af7670e6c3ee695cb37e6b3c7aa48858349210d6
vikaputinceva <vikaputin2008@gmail.com>
2026-02-05 00:48:23 +0700

    practic7

--- js/main.js
+++ js/main.js
@@ -3,26 +3,30 @@ let app = new Vue ({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery', 
         altText: "A pair of socks",
-        //link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
-        inStock: true,
-        //inventory: 100,
-        //onSale: true,
+        // link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        // inventory: 100,
+        onSale: true,
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [ 
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantImage: "../assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantImage: "../assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
+
             }
         ],
-        // sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        //sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
         cart: 0,
     },
@@ -33,9 +37,26 @@ let app = new Vue ({
             delCart() {
                 this.cart -= 1
             },
-            updateProduct(variantImage) {
-        this.image = variantImage
-        }
-        },
+            updateProduct(index) {
+                this.selectedVariant = index;
+            },
+    
+    },
+       computed: {
+                title() {
+                    return this.brand + ' ' + this.product;
+                        },
+                image() {
+                    return this.variants[this.selectedVariant].variantImage;
+                        },
+                inStock(){
+                    return this.variants[this.selectedVariant].variantQuantity
+                        },
+                sale(){
+                    return (this.onSale ? 'skidka net ' : 'skidka est ') 
+                    + ' this ' + this.product + ' for brand: ' + this.brand;
+                }
+
+            },
 
 })
